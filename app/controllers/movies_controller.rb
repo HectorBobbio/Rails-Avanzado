d7c52bba5bd16aaffbeb9faa888c7e5c14acaf61
Héctor Bobbio <85301034+HectorBobbio@users.noreply.github.com>
2023-12-13 08:00:32 -0500

    Update movies_controller.rb

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -35,7 +35,7 @@ class MoviesController < ApplicationController
   def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
-    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    flash[:notice] = "Movie '#{@movie.title}' deleted succesfully."
     redirect_to movies_path
   end
   private
